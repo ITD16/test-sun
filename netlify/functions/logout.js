f983d50a2912abfe0b2ccb70cb853da905cc9685
ITD16 <it.support@d16pro.com>
2026-03-19 11:53:25 +0700

    Update logout.js

--- netlify/functions/logout.js
+++ netlify/functions/logout.js
@@ -1,7 +1,11 @@
 const { json, clearSessionCookie } = require("./_utils");
 
 exports.handler = async () => {
-  return json(200, { ok: true }, {
-    "Set-Cookie": clearSessionCookie()
-  });
+  return json(
+    200,
+    { ok: true },
+    {
+      "Set-Cookie": clearSessionCookie()
+    }
+  );
 };
