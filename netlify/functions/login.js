1ea1d7625143d97d3b5bb8a08866d431e6e22389
ITD16 <it.support@d16pro.com>
2026-03-19 10:52:16 +0700

    Create login.js

---
+++ netlify/functions/login.js
@@ -0,0 +1,21 @@
+const { json, readUsers, setSessionCookie } = require("./_utils");
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    return json(405, { error: "Method not allowed" });
+  }
+
+  try {
+    const { username, password } = JSON.parse(event.body || "{}");
+    const users = readUsers();
+    const found = users.find(x => x.username === username && x.password === password && x.active !== false);
+
+    if (!found) return json(401, { error: "Invalid username or password" });
+
+    return json(200, { ok: true, username }, {
+      "Set-Cookie": setSessionCookie(username)
+    });
+  } catch (err) {
+    return json(500, { error: err.message || "Login error" });
+  }
+};
