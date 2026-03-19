6233db627500e30e05310dd892743c4dc2b60b6c
ITD16 <it.support@d16pro.com>
2026-03-19 10:53:16 +0700

    Create get-config.js

---
+++ netlify/functions/get-config.js
@@ -0,0 +1,15 @@
+const { json, authRequired, getRepoFile, repoInfo } = require("./_utils");
+
+exports.handler = async (event) => {
+  const auth = authRequired(event);
+  if (!auth.ok) return auth.response;
+
+  try {
+    const { configPath } = repoInfo();
+    const file = await getRepoFile(configPath);
+    const config = JSON.parse(file.content);
+    return json(200, { config });
+  } catch (err) {
+    return json(500, { error: err.message || "Cannot load config" });
+  }
+};
